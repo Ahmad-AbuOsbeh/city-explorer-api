d12e5281829b22e047832d12ecab1f4cf074fd37
Ahmad Abu Osbeh <ahmadabuosbeh20@gmail.com>
2021-05-31 01:16:10 +0300

    finished lab 09

--- server.js
+++ server.js
@@ -10,15 +10,18 @@ const axios = require('axios');
 server1.use(cors());
 // const PORT2 =3003;
 const PORT2 =process.env.PORT;
-const WEATHER_API_KEY=process.env.WEATHER_API_KEY;
-const MOVIE_API_KEY=process.env.MOVIE_API_KEY;
 
 server1.listen(PORT2,()=>{
     console.log(`i am listening on port ${PORT2}`);
 })
 
-server1.get('/ahmad',gettingWeather)
-server1.get('/movies',gettingMovies)
+const weather = require('./weather.js');
+const movies = require('./movies.js');
+
+server1.get('/ahmad',weather)
+server1.get('/movies',movies)
+
+
 
 // =>{
     // let x= 'hellooo from sever, yes i recieved your request(from any where). this is my response'
@@ -38,51 +41,12 @@ server1.get('/movies',gettingMovies)
     // res.send(x);
 // })
 
-function  gettingWeather(request,response) {
-    let x= 'hellooo from sever, yes i recieved your request(from any where). this is my response'
-    let requsetedCity=request.query.desired_city;
-    let weathUrlReq=`http://api.weatherbit.io/v2.0/current?city=${requsetedCity}&key=${WEATHER_API_KEY}`
-    // let wheatherApiResult= 
-    axios
-    .get(weathUrlReq)
-    .then(results=>{
-        let r=results.data.data[0];
-        console.log('resul.data.data',r);
-        let finalData=new Forecast(r);
-        response.status(200).send(finalData);
 
-    })
-    .catch(err=>{
-        response.status(500).send(`error in getting data ==> ${err}`)
-    })
-}
 
 //second request : for movies
 // https://api.themoviedb.org/3/search/movie?api_key=a774e112885b40790b2a79a1cd7e8d02&query=amman
-function gettingMovies(request,response) {
-    let requsetedMovie=request.query.desired_city;
-
-    let movieUrlReq=`https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${requsetedMovie}`;
-    axios
-    .get(movieUrlReq)
-    .then(results=>{
-        let movies=results.data.results;
-        response.send(movies);
-    })
-}
 
 
-class Forecast {
-
-    constructor(newInstance){
-        // this.descreption = `temp = ${newInstance.low_temp} C , wind speed = ${newInstance.max_temp} with ${newInstance.weather.description}`
-
-        // this.date= newInstance.valid_date;
-        this.descreption = `temp = ${newInstance.temp} C , wind speed = ${newInstance.wind_spd} with ${newInstance.weather.description}`
-
-        this.date= newInstance.datetime;
-    }
-}
 
 // class Movie{
 //     constructor(newInstance){
